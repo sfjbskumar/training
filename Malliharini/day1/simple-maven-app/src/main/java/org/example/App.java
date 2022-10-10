a53b0cce8fcd22d85b6436b7aaa8e70298ae2a99
sfjbs <malliharinikm@gmail.com>
2022-10-10 11:20:41 +0000

    Day1 Commit

---
+++ Malliharini/day1/simple-maven-app/src/main/java/org/example/App.java
@@ -0,0 +1,15 @@
+package org.example;
+
+
+
+public class App {
+    public  static void main(String[]args) {
+        System.out.println("hello");
+       Greeting greeting=new Greeting();
+       greeting.sayHello();
+       greeting.sayHi();
+       greeting.sayWelcome();
+       greeting.sayThanks();
+
+    }
+}
