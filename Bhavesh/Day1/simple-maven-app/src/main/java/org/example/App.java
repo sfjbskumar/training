b4e82fadb0c377cc2ba1c547356009b4b4e30e19
zeniiitsu <bhaveshainkar1234@gmail.com>
2022-10-10 13:05:30 +0000

    Day1 Commit

---
+++ Bhavesh/Day1/simple-maven-app/src/main/java/org/example/App.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.example2.Greeting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+        Greeting greeting = new Greeting();
+        greeting.sayHello();
+        greeting.sayHi();
+        greeting.sayWelcome();
+        greeting.sayThanks();
+    }
+}
