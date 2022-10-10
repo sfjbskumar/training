cd7971e0afe28870e2fea1bf14cc88afdce491a1
radhikadakhore <radhikadakhore19@gmail.com>
2022-10-10 12:13:31 +0000

    Day 1

---
+++ Radhika/Day1/untitled1/src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+        public class App
+        {
+            public static void main( String[] args )
+            {
+                System.out.println( "Hello World!" );
+                Greeting greeting=new Greeting();
+                greeting.sayHello();
+                greeting.sayHi();
+                greeting.sayWelcome();
+                greeting.sayThanks();
+            }
+        }
