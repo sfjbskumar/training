56d8dc0648e1955977830d483a6614a23a35996c
sfjbsoct6 <Sravs@nil1>
2022-10-10 10:12:33 +0000

    second commit and push to test git

--- k4j/Day1/simple-maven-app/src/main/java/com/kumar/data/App.java
+++ k4j/Day1/simple-maven-app/src/main/java/com/kumar/data/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hi!Hello" );
         Greeting greeting=new Greeting();
         greeting.sayHello();
         greeting.sayHi();
