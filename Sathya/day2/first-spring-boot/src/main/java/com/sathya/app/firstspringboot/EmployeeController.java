e894622b59f8563ca7c6dec4681579b2083dc02e
sfjbs <sathya572001@gmail.com>
2022-10-11 08:32:16 +0000

    sathya 1st day

---
+++ Sathya/day2/first-spring-boot/src/main/java/com/sathya/app/firstspringboot/EmployeeController.java
@@ -0,0 +1,11 @@
+package com.sathya.app.firstspringboot;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class EmployeeController {
+    @GetMapping("hi")
+    public String greeting() {
+        return "Hello";
+    }
+}
