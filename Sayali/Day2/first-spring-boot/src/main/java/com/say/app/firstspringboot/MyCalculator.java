ca2d922f066deec68d8e9d7c0071e86e1c0dac4f
ss1234-p <shravagis@gmail.com>
2022-10-12 12:52:05 +0000

    committed

---
+++ Sayali/Day2/first-spring-boot/src/main/java/com/say/app/firstspringboot/MyCalculator.java
@@ -0,0 +1,10 @@
+package com.say.app.firstspringboot;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyCalculator {
+    public String addString(String a,String b){
+        return a.concat(b);
+    }
+}
