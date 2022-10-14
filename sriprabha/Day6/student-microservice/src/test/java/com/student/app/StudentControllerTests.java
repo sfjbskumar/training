74897cc28460a89ebf52f3edc270ee20c28b930c
tadivakasriprabha <tadivakasriprabha@gmail.com>
2022-10-14 09:56:07 +0000

    day6

--- sriprabha/Day6/student-microservice/src/test/java/com/student/app/StudentControllerTests.java
+++ sriprabha/Day6/student-microservice/src/test/java/com/student/app/StudentControllerTests.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
