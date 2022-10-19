61478613064888d2da9f1f8b7291458d781e618a
sfjbsoct6 <Sravs@nil1>
2022-10-19 06:33:28 +0000

    day9

--- k4j/Day9_New/demo/src/main/java/com/kumar/demo/repository/StudentRepository.java
+++ k4j/Day9_New/demo/src/main/java/com/kumar/demo/repository/StudentRepository.java
@@ -2,7 +2,7 @@ package com.kumar.demo.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import com.kumar.demo.model.Student;
-@Repository
+
 public interface StudentRepository extends CrudRepository<Student, Integer>
 {
 }
