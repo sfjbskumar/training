1400bcf6687a6208ea3bd10daf823b5711f84cc1
sfjbs <malliharinikm@gmail.com>
2022-10-21 07:07:18 +0000

    spring-rest-communication commit

---
+++ Malliharini/day10/spring-rest-communication/student-microservice-mysql/src/main/java/com/harini/demo/service/StudentService.java
@@ -0,0 +1,37 @@
+package com.harini.demo.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.harini.demo.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.harini.demo.model.Student;
+
+@Service
+public class StudentService
+{
+    @Autowired
+    StudentRepository studentRepository;
+    //getting all student records
+    public List<Student> getAllStudent()
+    {
+        List<Student> students = new ArrayList<Student>();
+        studentRepository.findAll().forEach(student -> students.add(student));
+        return students;
+    }
+    //getting a specific record
+    public Student getStudentById(int id)
+    {
+        return studentRepository.findById(id).get();
+    }
+    public void saveOrUpdate(Student student)
+    {
+        studentRepository.save(student);
+    }
+    //deleting a specific record
+    public void delete(int id)
+    {
+        studentRepository.deleteById(id);
+    }
+}
