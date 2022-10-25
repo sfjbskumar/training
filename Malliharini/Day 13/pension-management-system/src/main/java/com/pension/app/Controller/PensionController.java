3fcad22e4f77a53dea09c575d4a1a4eac014f324
sfjbs <malliharinikm@gmail.com>
2022-10-25 10:19:33 +0000

    mini project commit

--- Malliharini/Day 13/pension-management-system/src/main/java/com/pension/app/Controller/PensionController.java
+++ Malliharini/Day 13/pension-management-system/src/main/java/com/pension/app/Controller/PensionController.java
@@ -1,5 +1,6 @@
 package com.pension.app.Controller;
 
+import com.pension.app.model.Pension;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -7,6 +8,33 @@ import java.util.Map;
 
 @RestController
 public class PensionController {
+    private Map<String, Pension> pensionMap= new HashMap<String,Pension>();
 
+  /*  @GetMapping("/get/studentapp")
+    public Pension getStudent(@RequestParam String name){
+        Pension result= pensionMap.get(name);
+        return result;
+    }*/
+    @PostMapping("create/pension")
+    public void SaveStudent(@RequestBody Pension pension){
+        String name= pension.getApplicant_name();
+        pensionMap.put(name,pension);
+    }
+    @PutMapping("edit/pension")
+    public Pension updateStudent(@RequestParam String applicant_id,@RequestParam String applicant_name){
+        Pension result= pensionMap.get(applicant_id);
+        result.setApplicant_name(applicant_name);
+        pensionMap.put(applicant_name,result);
+        return result;
 
+    }
+ /*   @GetMapping("/get/all")
+    public Map<String,Student> getStudent(){
+        return studentMap;}
+
+    @DeleteMapping("remove/studentapp")
+    public void deleteStudent(@RequestParam String name){
+        studentMap.remove(name);
+    }
+*/
 }
--- Malliharini/Day 13/pension-management-system/src/test/java/com/pension/app/PensionMicroserviceApplicationTests.java
+++ Malliharini/Day 13/pension-management-system/src/test/java/com/pension/app/PensionMicroserviceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PensionMicroserviceApplicationTests {
 
-	/*@Test
+	@Test
 	void contextLoads() {
-	}*/
+	}
 
 }
