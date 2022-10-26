778fa212a7a889f9f9b21e2047e22f9a2978bf8b
piyushramteke <piyushramteke401@gmail.com>
2022-10-26 08:43:10 +0530

    Piyush commit

---
+++ Piyush/Day3/Calculator-spring-boot/Calculator-spring-boot/src/test/java/com/piyush/Calculatorspringboot/CalculatorSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piyush.Calculatorspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
