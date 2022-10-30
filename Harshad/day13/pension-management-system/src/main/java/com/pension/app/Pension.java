3eb867dff1ad8e263f50a53ea83cf1cfc5bfcdc8
Sesha-Gayathri1 <seshagayathri12345@gmail.com>
2022-10-30 11:25:16 +0000

    commit5

--- Harshad/day13/pension-management-system/src/main/java/com/pension/app/Pension.java
+++ Harshad/day13/pension-management-system/src/main/java/com/pension/app/Pension.java
@@ -103,3 +103,45 @@ public class Pension {
         this.installment=installment;
     }
 }
+
+
+
+pensionservice Test
+
+ @Autowired
+    private PensionService pensionService;
+    List<PensionModel> p = new ArrayList<PensionModel>();
+
+    @MockBean
+    private PensionRepository pensionRepository;
+
+    @Test
+    public void getAllApplicantsTest() {
+
+
+        when(pensionRepository.findAll()).thenReturn(Stream.of(new PensionModel(2, "Vasanti", 45, 8000, "7684925400", "W", "N", "0722", 1000)).collect(Collectors.toList()));
+        assertEquals(1, pensionService.getAllPension().size());
+    }
+
+    @Test
+    public void getPensionByIDTest() {
+
+        PensionModel p1 = new PensionModel(1, "Vani", 72, 8000, "7684925464", "R", "Y", "09/22", 1000);
+        p.add(p1);
+        PensionModel p2 = new PensionModel(3, "ravi", 89, 8000, "7684925400", "R", "N", "07/22", 1000);
+        p.add(p2);
+
+
+        when(pensionRepository.findById(p.get(0).getId())).thenReturn(Optional.of(p.get(0)));
+        assertEquals(1, pensionService.getPensionById(1).getId());
+    }
+
+    @Test
+    public void deleteTest() {
+
+
+        PensionModel p2 = new PensionModel(3, "ravi", 89, 8000, "7684925400", "R", "N", "07/22", 1000);
+
+        doNothing().when(pensionRepository).delete(p2);
+    }
+}
--- SeshaGayathri/Day9_New/demo-MySQL/src/main/java/com/sagar/demo/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sagar.demo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.sagar.demo.model.Student;
-public interface StudentRepository extends CrudRepository<Student, Integer>
-{
-}
