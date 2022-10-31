7c9abb595f8e887d03496f994d7fb14d7e6b7bbb
Ankita Shanware <ankitashanware01@gmail.com>
2022-10-31 08:07:40 +0000

    Committed.

---
+++ Ankita/pension-management-system/src/main/java/com/pension/app/PensionRepository.java
@@ -0,0 +1,24 @@
+package com.pension.app;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Modifying;
+
+import org.springframework.data.repository.CrudRepository;
+import com.pension.app.Pension;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.parameters.P;
+
+import java.util.List;
+public interface PensionRepository extends CrudRepository<Pension, Integer>
+{
+//    @Modifying
+//    @Query(value="update pension set name=:k where empstatus =:n and penstatus =:m", nativeQuery = true)
+//     void updatePensionStatus(@Param("k") String name, @Param("n") String empstatus, @Param("m") String penstatus);
+
+    @Query(value = "select * from pension",nativeQuery = true)
+    public List<Pension> getPensions();
+
+    @Modifying
+    @Query(value="update Pension set name=:'jish kumar' where empstatus ='R' and penstatus ='Y' ", nativeQuery = true)
+    void updatePensionStatus();
+}
