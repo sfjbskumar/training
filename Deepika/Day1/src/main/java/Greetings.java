f5f642c08aa69d5d69ea8fe3a3a0d48a03373887
honkandeepika <96882826+honkandeepika@users.noreply.github.com>
2022-11-01 10:58:16 +0000

    Add files via upload

---
+++ Deepika/Day1/src/main/java/Greetings.java
@@ -0,0 +1,15 @@
+public class Greetings {
+    public String sayHello(){
+        return "Hello";
+    }
+
+    public String sayHi(){
+        return "Hi";
+    }
+    public String sayWelcome(){
+        return "Welcome";
+    }
+    public String sayThanks(){
+        return "Thanks";
+    }
+}
